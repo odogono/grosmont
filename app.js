7b1da884ad60ffafcb548de80ac2cd9e7b373efd
Alex Veenendaal <alex@opendoorgonorth.com>
2014-11-06 23:05:57 +0000

    initial checkin

---
+++ app.js
@@ -0,0 +1,10 @@
+require('./ghost/index.js');
+process.chdir('./ghost');
+
+/*var http = require('http');
+var server = http.createServer(function(req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end("hello edit.world!\n");
+});
+server.listen(3000);
+//*/
