6578f1f040c42ade6b29d31ff408f219498df280
Alex Veenendaal <alex@opendoorgonorth.com>
2020-02-25 11:05:14 +0000

    who even knows

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,69 +1,45 @@
-import React from 'react';
-import Link from 'gatsby-link';
-import { Container } from 'react-responsive-grid';
+import React from 'react'
+import PropTypes from 'prop-types'
+import Helmet from 'react-helmet'
 
-import { rhythm, scale } from '../utils/typography';
+import Header from '../components/header'
+import './index.css'
 
-class Template extends React.Component {
-    render() {
-        const { location, children } = this.props;
-        let header;
-        if (location.pathname === '/') {
-            header = (
-                <h1
-                    style={{
-                        ...scale(1.0),
-                        marginBottom: rhythm(1.5),
-                        marginTop: 0
-                    }}>
-                    <Link
-                        style={{
-                            boxShadow: 'none',
-                            textDecoration: 'none',
-                            color: 'inherit'
-                        }}
-                        to={'/'}>
-                        Open Door Go North
-                    </Link>
-                </h1>
-            );
-        } else {
-            header = (
-                <h3
-                    style={{
-                        fontFamily: 'Clear Sans, sans-serif',
-                        marginTop: 0,
-                        marginBottom: rhythm(-1)
-                    }}>
-                    <Link
-                        style={{
-                            boxShadow: 'none',
-                            textDecoration: 'none',
-                            color: 'inherit'
-                        }}
-                        to={'/'}>
-                        Open Door Go North
-                    </Link>
-                </h3>
-            );
-        }
-        return (
-            <Container
-                style={{
-                    maxWidth: rhythm(24),
-                    padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`
-                }}>
-                {header}
-                {children()}
-            </Container>
-        );
-    }
+const Layout = ({ children, data }) => (
+  <div>
+    <Helmet
+      title={data.site.siteMetadata.title}
+      meta={[
+        { name: 'description', content: 'Sample' },
+        { name: 'keywords', content: 'sample, something' },
+      ]}
+    />
+    <Header siteTitle={data.site.siteMetadata.title} />
+    <div
+      style={{
+        margin: '0 auto',
+        maxWidth: 960,
+        padding: '0px 1.0875rem 1.45rem',
+        paddingTop: 0,
+      }}
+    >
+      {children()}
+    </div>
+  </div>
+)
+
+Layout.propTypes = {
+  children: PropTypes.func,
 }
 
-Template.propTypes = {
-    children: React.PropTypes.function,
-    location: React.PropTypes.object,
-    route: React.PropTypes.object
-};
+export default Layout
 
-export default Template;
+export const query = graphql`
+  query SiteTitleQuery {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`
