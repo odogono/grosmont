48699b98c02b1d59e897b89bec766f4a14c051d4
Alex Veenendaal <alex@opendoorgonorth.com>
2020-05-20 12:37:58 +0100

    removal of comments, and starting to deal with config frontmatter

--- README.md
+++ README.md
@@ -16,3 +16,4 @@ https://news.ycombinator.com/item?id=23228904
 
 https://leerob.io/blog/mdx
 
+https://preactjs.com/guide/v10/server-side-rendering
--- src/transpile.tsx
+++ src/transpile.tsx
@@ -5,44 +5,42 @@ import React from 'react';
 import ReactDOMServer from 'react-dom/server';
 import * as Babel from "@babel/core";
 const _eval = require('eval');
-import {mdx, MDXProvider} from '@mdx-js/react'
+import { mdx, MDXProvider } from '@mdx-js/react'
 const emoji = require('remark-emoji')
 const frontmatter = require('remark-frontmatter');
-const visit = require('unist-util-visit')
-const { mdx: createElement} = require('@mdx-js/react')
-const {renderToStaticMarkup} = require('react-dom/server')
-
+import unistVisit from 'unist-util-visit';
+import unistRemove from 'unist-util-remove';
 
 const presets = [
-  // ["latest-node", { "target": "current" }],
-  [ "@babel/preset-env", { 
-      "exclude":[
-          "@babel/plugin-transform-spread"
-      ], 
-      "targets": { "node": "current" } 
-  } ],
-  "@babel/preset-react"
+    // ["latest-node", { "target": "current" }],
+    ["@babel/preset-env", {
+        "exclude": [
+            "@babel/plugin-transform-spread"
+        ],
+        "targets": { "node": "current" }
+    }],
+    "@babel/preset-react"
 ];
 
 
-export async function transpile(path:string){
-    
+export async function transpile(path: string) {
+
     const Component = readToElements(path);
 
     // const fn = new Function('_fn', 'React', `return ${code}`);
 
     const components = {
-        wrapper: ({children, ...props}) => {
-        //   console.log(children.map(child => child.props.mdxType))
-          return <>{children}</>
+        wrapper: ({ children, ...props }) => {
+            //   console.log(children.map(child => child.props.mdxType))
+            return <>{children}</>
         },
-        Something: ({children, ...props}) => <div className="blnk">{children}</div>
+        Something: ({ children, ...props }) => <div className="blnk">{children}</div>
     }
 
-    const html = ReactDOMServer.renderToStaticMarkup( 
+    const html = ReactDOMServer.renderToStaticMarkup(
         <MDXProvider components={components}>
             <Component />
-        </MDXProvider> );
+        </MDXProvider>);
     // console.log(html);
 
     return html;
@@ -50,44 +48,79 @@ export async function transpile(path:string){
     // console.log('out = ', mdx("h1", null, `Hello, world!`) );
 }
 
-function readToElements(path:string){
+function readToElements(path: string) {
     const content = Fs.readFileSync(path, 'utf8');
 
-    const VisitPlugin = () => (tree, file) => {
-        visit(tree, 'heading', node => {
-          console.log('[VisitPlugin]', node);
-        })
-    };
-    // const FMPlugin = frontmatter(['toml']);
+    // this is probably the key to extracting data from the mdx
 
     const options = {
         filepath: path,
-        remarkPlugins: [/*VisitPlugin,*/ emoji, [frontmatter, {type: 'custom', marker: '+'}]],
+        remarkPlugins: [
+            emoji,
+            [frontmatter, { type: 'config', marker: '+' }],
+            configPlugin,
+            removeCommentPlugin,
+        ],
         // skipExport: true
-      };
+    };
 
     let jsx = Mdx.sync(content, options);
 
     console.log('jsx', jsx);
     let code = transformJSX(jsx);
-    const el = buildElement(code,path);
+    const el = buildElement(code, path);
 
     return el;
 }
 
-function transformJSX(jsx:string){
-    return Babel.transform(jsx, {presets}).code;
+function transformJSX(jsx: string) {
+    return Babel.transform(jsx, { presets }).code;
 }
 
-function buildElement(code:string, path:string){
+function buildElement(code: string, path: string) {
     const require = (requirePath) => {
-        const fullPath = Path.resolve( Path.dirname(path), requirePath );
+        const fullPath = Path.resolve(Path.dirname(path), requirePath);
         return readToElements(fullPath);
     }
-    let ed = _eval( code, path, {mdx,require} );
+    let ed = _eval(code, path, { mdx, require });
 
-    console.log('[buildElement]', ed );
+    console.log('[buildElement]', ed);
 
     return ed.default;
 }
 
+
+
+function removeTypePlugin(options = {}) {
+    return (tree, file) => {
+        // console.log('[removeTypePlugin]', file);
+        unistRemove(tree, options);
+    }
+}
+
+function configPlugin() {
+    return (tree, file) => {
+        unistVisit(tree, { type: 'config' }, (node, index, parent) => {
+
+            // TODO : convert this into a javascript call
+
+            // console.log('[configPlugin]', node);// (node as any).value);
+        })
+        unistRemove(tree, 'config');
+    }
+};
+
+function removeCommentPlugin() {
+    return (tree, file) => {
+        unistRemove(tree, {cascade:false}, (node, idx, parent) => {
+            if( node.type === 'paragraph' ){
+                const exists = node.children.filter( node => node.type === 'text' && node.value.trim().startsWith('//') );
+                if( exists.length > 0 ){
+                    // console.log('[removeCommentPlugin]', node);
+                    return true;
+                }
+            }
+            return false;
+        });
+    }
+};
