9e5777df0ef96368d1d15850632ca287bd052c0c
Alex Veenendaal <alex@opendoorgonorth.com>
2021-03-01 15:06:28 +0000

    refactor of scss processor

--- src/builder/processor/scss/index.ts
+++ src/builder/processor/scss/index.ts
@@ -10,10 +10,11 @@ import { Entity, EntityId } from "odgn-entity/src/entity";
 
 import { Site } from '../../site';
 import { ProcessOptions } from '../../types';
-import { selectScss, FindEntityOptions, getDstUrl } from '../../query';
-import { info, setLocation } from '../../reporter';
+import { selectScss, FindEntityOptions, getDstUrl, selectSrcByMime } from '../../query';
+import { info, error, setLocation } from '../../reporter';
 import { printEntity } from 'odgn-entity/src/util/print';
 import { joinPaths } from '../../util';
+import { Component, getComponentEntityId, setEntityId } from 'odgn-entity/src/component';
 
 
 
@@ -22,43 +23,56 @@ import { joinPaths } from '../../util';
  * putting the result into a text component
  * 
  */
-export async function process(site: Site, options:ProcessOptions = {}) {
+export async function process(site: Site, options: ProcessOptions = {}) {
     const es = options.es ?? site.es;
-    const {reporter} = options;
-    setLocation( reporter, '/processor/scss' );
+    const { reporter } = options;
+    setLocation(reporter, '/processor/scss');
 
     // select scss entities
-    const ents = await selectScss(es, options);
+    // const ents = await selectScss(es, options);
+    const srcComs = await selectSrcByMime(es, ['text/scss'], options);
+    let addComs = [];
 
-    for (let e of ents) {
+    for (let srcCom of srcComs) {
+        const eid = getComponentEntityId(srcCom);
+        const { url } = srcCom;
 
         try {
-            const {css, srcPath, dstPath} = await renderScss( site, e, options );
-            e.Text = { data:css, mime: 'text/css' };
-            
-    
+            const { css, srcPath, dstPath } = await renderScss(site, srcCom, options);
+            // e.Text = { data: css, mime: 'text/css' };
+
+            let com = es.createComponent( '/component/text', {data:css, mime:'text/css'} );
+            addComs.push( setEntityId(com, eid) );
+
+
+
             // alter the target filename
-            const url = e.Src.url;
-            let filename = Path.basename(url);
-    
-            // const dstUrl = await getDstUrl(es, e.id);
-    
-            // log('dstUrl', dstUrl);
-    
-            filename = filename.substr(0, filename.lastIndexOf(".")) + ".css";
-            e.Dst = { url:filename };
+            // const url = e.Src.url;
+            // let filename = Path.basename(url);
 
-            info( reporter, url, {eid:e.id} );
+            let dstUrl = await getDstUrl(es, eid);
 
-        } catch( err ){
-            e.Error = {message:err.message, stack:err.stack};
-            reporter.error(err.message, err);
+            if( dstUrl !== undefined ){
+                let filename = dstUrl.substr(0, dstUrl.lastIndexOf(".")) + ".css";
+    
+                com = es.createComponent( '/component/dst', {url:filename} );
+                addComs.push( setEntityId(com, eid) );
+            }
+
+
+            info(reporter, url, { eid: eid });
+
+        } catch (err) {
+            let com = es.createComponent('/component/error', { message: err.message, stack: err.stack });
+            addComs.push( setEntityId(com, eid) );
+            error( reporter, err.message, err);
             throw err;
         }
     }
 
     // apply changes
-    await es.add( ents );
+    // await es.add(ents);
+    await es.add( addComs );
 
     return site;
 }
@@ -82,47 +96,39 @@ interface RenderScssResult {
  * @param es 
  * @param e 
  */
-export async function renderScss(site: Site, e: Entity, options:ProcessOptions = {}): Promise<RenderScssResult> {
-    if (e.Scss === undefined) {
-        return { css: undefined, srcPath: undefined, dstPath: undefined };
-    }
+export async function renderScss(site: Site, srcCom: Component, options: ProcessOptions = {}): Promise<RenderScssResult> {
+    // if (e.Scss === undefined) {
+    //     return { css: undefined, srcPath: undefined, dstPath: undefined };
+    // }
 
-    const dst = await getDstUrl(site.es, e.id);
-    let dstUrl = site.getDstUrl( dst );
+    const eid = getComponentEntityId(srcCom);
 
-    const filename = getSrcUrl(e);
+    const dst = await getDstUrl(site.es, eid);
+    let dstUrl = site.getDstUrl(dst);
+
+    // const filename = getSrcUrl(e);
+    const { url } = srcCom;
+    const filename = Path.basename(url);
 
 
     let dstPath = joinPaths(dstUrl, filename);
 
+    const scss = await site.getEntityData(eid);
 
     const srcPath = '/';
-    let scss = e.Scss?.data;
-    if( scss === undefined ){
-        scss = await site.readUrl( e.Src?.url );
-        if( scss === undefined ){
-            throw new Error(`scss data not found for ${e.id}`);
-        }
+    // let scss = e.Scss?.data;
+    // if( scss === undefined ){
+    // scss = await site.readUrl( e.Src?.url );
+    if (scss === undefined) {
+        throw new Error(`scss data not found for ${eid}`);
     }
+    // }
 
     const css = await render(scss, srcPath, dstPath, true);
 
     return { css, srcPath, dstPath };
 }
 
-
-/**
- * 
- * @param e 
- */
-function getSrcUrl(e: Entity) {
-    if (e.Src !== undefined) {
-        const url = e.Src.url;
-        return Path.basename(url);
-    }
-
-    return undefined;
-}
 
 
 async function render(data: string, srcPath: string, dstPath: string, minify: boolean = false) {
