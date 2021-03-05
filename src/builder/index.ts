f3ec62f4b255b0fc0005af665b3c50f2a00b8841
Alex Veenendaal <alex@opendoorgonorth.com>
2021-03-05 12:05:17 +0000

    converted to use data driven array of processors with priorities rather than hardcoded

--- src/builder/index.ts
+++ src/builder/index.ts
@@ -1,4 +1,4 @@
-
+import Jsonpointer from 'jsonpointer';
 import { Site, SiteOptions } from './site';
 import { process as scanSrc } from './processor/file';
 import { process as evalJsx } from './processor/jsx/eval_jsx';
@@ -16,11 +16,15 @@ import { process as buildDstIndex } from './processor/dst_index';
 import { EntityUpdate, ProcessOptions } from './types';
 import { buildSrcIndex, clearUpdates } from './query';
 
+const Label = '/build';
+const log = (...args) => console.log(`[${Label}]`, ...args);
 
 export interface BuildProcessOptions extends ProcessOptions {
     updates?: EntityUpdate[];
 }
 
+
+type ProcessorEntry = [ Function, number, any? ];
 
 /**
  * 
@@ -32,53 +36,92 @@ export async function build(site: Site, options: BuildProcessOptions = {}) {
     const siteRef = site.getRef();
     const updateOptions = { reporter, onlyUpdated: true, ...options, siteRef };
 
-    
-    // clear /component/update from site
-    await clearUpdates(site.es, { siteRef });
-    
-    await scanSrc(site, updateOptions); //{...options, reporter, siteRef});
+    let siteE = site.getEntity();
+    let config = Jsonpointer.get(siteE, '/Meta/meta/processors');
+
     
     
+    let processors:ProcessorEntry[] = [
+        [ clearUpdates,     1000 ],
+        [ scanSrc,          0 ],
+        [ mark,             0, { exts: ['html', 'jpeg', 'jpg', 'png', 'svg', 'txt'], comUrl: '/component/static' } ],
+        [ mark,             0, { exts: ['jsx', 'tsx'], comUrl: '/component/jsx', mime: 'text/jsx' } ],
+        [ mark,             0, { exts: ['mdx'], comUrl: '/component/mdx', mime: 'text/mdx' } ],
+        [ mark,             0, { exts: ['scss'], comUrl: '/component/scss', mime: 'text/scss' } ],
+        [ buildSrcIndex,    0 ],
+        [ renderScss,       0, {renderScss:true} ],
+        [ evalJsx,          0 ],
+        [ evalMdx,          0 ],
+        [ applyTags,        0 ],
+        [ evalJs,           0 ],
+        [ resolveMeta,      0 ],
+        [ buildDstIndex,    0, {url:'/processor/build_dst_index'} ],
+        [ renderJs,         0 ],    
+        [ buildDstIndex,    -99 ],
+        [ write,            -100 ],
+        [ copyStatic,       -101 ],
+    ];
+
+    const loaded = await parseProcessorConfig(config);
+    processors = processors.concat(loaded);
+
+    // sort according to priority descending
+    processors.sort( ([a,ap],[b,bp]) => {
+        if( ap < bp ){ return 1; }
+        if( ap > bp ){ return -1; }
+        return 0;
+    });
+
     
+    for( let [ prc, priority, options ] of processors ){
+        options = options ?? {};
+        await prc( site, {...updateOptions, ...options} );
+    }
+
+    
+    // // clear /component/update from site
+    // await clearUpdates(site, { siteRef });
+    
+    // await scanSrc(site, updateOptions); //{...options, reporter, siteRef});
     
 
-    // await markStatic(site, updateOptions);
+    // // await markStatic(site, updateOptions);
 
-    await mark(site, { ...options, exts: ['html', 'jpeg', 'jpg', 'png', 'svg', 'txt'], comUrl: '/component/static' });
-    await mark(site, { ...options, exts: ['jsx', 'tsx'], comUrl: '/component/jsx', mime: 'text/jsx' })
-    await mark(site, { ...options, exts: ['mdx'], comUrl: '/component/mdx', mime: 'text/mdx' })
-    await mark(site, { ...options, exts: ['scss'], comUrl: '/component/scss', mime: 'text/scss' })
+    // await mark(site, { ...options, exts: ['html', 'jpeg', 'jpg', 'png', 'svg', 'txt'], comUrl: '/component/static' });
+    // await mark(site, { ...options, exts: ['jsx', 'tsx'], comUrl: '/component/jsx', mime: 'text/jsx' })
+    // await mark(site, { ...options, exts: ['mdx'], comUrl: '/component/mdx', mime: 'text/mdx' })
+    // await mark(site, { ...options, exts: ['scss'], comUrl: '/component/scss', mime: 'text/scss' })
 
-    await buildSrcIndex(site);
+    // await buildSrcIndex(site);
 
-    await renderScss(site, updateOptions);
+    // await renderScss(site, updateOptions);
 
-    await evalJsx(site, updateOptions);
+    // await evalJsx(site, updateOptions);
 
-    // creates a /component/js with the data
-    await evalMdx(site, updateOptions);
+    // // creates a /component/js with the data
+    // await evalMdx(site, updateOptions);
 
-    await applyTags(site, updateOptions);
-    // if( true ){
+    // await applyTags(site, updateOptions);
+    // // if( true ){
 
-    // evaluates the js, and returns metadata
-    await evalJs(site, updateOptions);
+    // // evaluates the js, and returns metadata
+    // await evalJs(site, updateOptions);
 
-    // resolve meta with parents
-    await resolveMeta( site, updateOptions );
+    // // resolve meta with parents
+    // await resolveMeta( site, updateOptions );
 
-    await buildDstIndex(site, updateOptions);
+    // await buildDstIndex(site, updateOptions);
 
-    // renders the js to /component/output
-    await renderJs(site, updateOptions);
+    // // renders the js to /component/output
+    // await renderJs(site, updateOptions);
 
-    await assignTitle(site, updateOptions);
+    // await assignTitle(site, updateOptions);
 
-    await buildDstIndex(site, updateOptions);
+    // await buildDstIndex(site, updateOptions);
 
-    await write(site, updateOptions);
+    // await write(site, updateOptions);
 
-    await copyStatic(site, updateOptions);
+    // await copyStatic(site, updateOptions);
 
     return site;
 }
@@ -86,3 +129,26 @@ export async function build(site: Site, options: BuildProcessOptions = {}) {
 
 
 
+async function parseProcessorConfig( config:any[] ){
+    let result:ProcessorEntry[] = [];
+
+    for( const entry of config ){
+        for( const pUrl of Object.keys(entry) ){
+            let { priority, ...options } = entry[pUrl];
+            log('proc', pUrl, priority, options );
+
+            try {
+                const { process } = await import( './' + pUrl );
+
+                result.push( [ process, priority, options ] );
+                
+            } catch( err ){
+                log('[parseProcessorConfig]', 'not found', pUrl );
+            }
+
+            // log(pUrl, module);
+        }
+    }
+
+    return result;
+}
