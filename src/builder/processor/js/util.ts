245caa75f44d7c5f451ed0ee19cb58584012f77e
Alex Veenendaal <alex@opendoorgonorth.com>
2021-04-02 20:41:31 +0100

    cleanup

--- src/builder/processor/js/util.ts
+++ src/builder/processor/js/util.ts
@@ -1,10 +1,10 @@
 import Util from 'util';
 import Path from 'path';
-import { 
+import {
     getComponentDefId, getComponentEntityId, toComponentId,
     Entity, EntityId,
     QueryableEntitySet,
-    StatementArgs 
+    StatementArgs
 } from "../../../es";
 import { applyMeta, buildUrl, resolveUrlPath, uriToPath } from "../../util";
 import { PageLink, PageLinks, SiteIndex, TranspileProps, ProcessOptions, DependencyType, EvalContext, ImportDescr } from "../../types";
@@ -17,7 +17,7 @@ import { info } from '../../reporter';
 
 const log = (...args) => console.log('[/processor/mdx/util]', ...args);
 
-export async function buildProps(site:Site, e: Entity): Promise<TranspileProps> {
+export async function buildProps(site: Site, e: Entity): Promise<TranspileProps> {
 
     let data = await site.getEntityData(e);
 
@@ -36,17 +36,17 @@ export async function buildProps(site:Site, e: Entity): Promise<TranspileProps> 
  * @param options 
  * @returns 
  */
-export function createRenderContext( site:Site, e:Entity, options:ProcessOptions = {} ): EvalContext {
+export function createRenderContext(site: Site, e: Entity, options: ProcessOptions = {}): EvalContext {
     const { url: base } = e.Src;
-    const {reporter}  = site;
+    const { reporter } = site;
 
-    const log = (...args) => info(reporter, Util.format(`[${base}]`,...args) );
+    const log = (...args) => info(reporter, Util.format(`[${base}]`, ...args));
 
-    const context = { 
+    const context = {
         e,
         page: e,
         es: site.es,
-        site, 
+        site,
         log,
         useServerEffect,
         processEntity: processEntityOutput(site, e, options),
@@ -58,73 +58,73 @@ export function createRenderContext( site:Site, e:Entity, options:ProcessOptions
 }
 
 
-function runQuery( site:Site, e:Entity, options:ProcessOptions ){
-    return async (q:string, args:StatementArgs) => {
-        const {es} = site;
+function runQuery(site: Site, e: Entity, options: ProcessOptions) {
+    return async (q: string, args: StatementArgs) => {
+        const { es } = site;
         let result = await es.prepare(q).getResult(args);
         // log('[runQuery]', q, result);
         return result;
     }
 }
 
-function processEntities( site:Site, e:Entity, options:ProcessOptions ){
-    return async (eids:EntityId[], dids:string[], renderOptions:ProcessOptions = {}) => {
-        const {es} = site;
+function processEntities(site: Site, e: Entity, options: ProcessOptions) {
+    return async (eids: EntityId[], dids: string[], renderOptions: ProcessOptions = {}) => {
+        const { es } = site;
         const srcId = e.id;
-        const bf = es.resolveComponentDefIds( dids );
+        const bf = es.resolveComponentDefIds(dids);
         let pes = new OutputES(es, bf);
 
-        const process = await buildProcessors( site, [
-            [ '/processor/js/render', 0, renderOptions]
+        const process = await buildProcessors(site, [
+            ['/processor/js/render', 0, renderOptions]
         ]);
 
 
-        await process( site, {es:pes as any, eids});
+        await process(site, { es: pes as any, eids });
 
-        let result:Map<EntityId,Entity> = new Map<EntityId,Entity>();
+        let result: Map<EntityId, Entity> = new Map<EntityId, Entity>();
 
-        for( const eid of eids ){
-            await insertDependency( es, srcId, eid, 'import' );
+        for (const eid of eids) {
+            await insertDependency(es, srcId, eid, 'import');
             let e = await es.getEntity(eid, bf);
             result.set(eid, e);
         }
 
         // apply the updated components from the output es to entities
-        for( const com of pes.components ){
+        for (const com of pes.components) {
             const eid = getComponentEntityId(com);
             const e = result.get(eid);
-            if( e !== undefined ){
+            if (e !== undefined) {
                 e.addComponentUnsafe(com);
             }
         }
 
-        return Array.from( result.values() );
+        return Array.from(result.values());
     }
 }
 
-function processEntityOutput( site:Site, e:Entity, options:ProcessOptions ){
+function processEntityOutput(site: Site, e: Entity, options: ProcessOptions) {
 
-    return async (url:string, renderOptions:ProcessOptions = {} ) => {
-        const {es} = site;
-        
+    return async (url: string, renderOptions: ProcessOptions = {}) => {
+        const { es } = site;
+
         let pe = await site.getEntityBySrc(url);
         const bf = es.resolveComponentDefIds('/component/output');
         let pes = new OutputES(es, bf);
 
-        const process = await buildProcessors( site, [
-            [ '/processor/js/render', 0, renderOptions]
+        const process = await buildProcessors(site, [
+            ['/processor/js/render', 0, renderOptions]
         ]);
-        const eids = [ pe.id ];
+        const eids = [pe.id];
 
         // add a dependency - since this is an import, it gets
         // reset at the point of eval_mdx/jsx
-        await insertDependency( es, e.id, pe.id, 'import' );
-        
-        await process( site, {es:pes as any, eids} );
+        await insertDependency(es, e.id, pe.id, 'import');
 
-        for( const com of pes.components ){
-            if( getComponentEntityId(com) === pe.id ){
-                e.addComponentUnsafe( com );
+        await process(site, { es: pes as any, eids });
+
+        for (const com of pes.components) {
+            if (getComponentEntityId(com) === pe.id) {
+                e.addComponentUnsafe(com);
             }
         }
 
@@ -134,20 +134,20 @@ function processEntityOutput( site:Site, e:Entity, options:ProcessOptions ){
 
 
 
-export async function buildPageLinks( es:QueryableEntitySet, linkIndex:SiteIndex ){
-    let result:PageLinks = new Map<string,PageLink>();
+export async function buildPageLinks(es: QueryableEntitySet, linkIndex: SiteIndex) {
+    let result: PageLinks = new Map<string, PageLink>();
 
-    
-    for( const [url, [eid,type,child]] of linkIndex.index ){
-        if( type === 'external' ){
-            result.set(url, {url});
+
+    for (const [url, [eid, type, child]] of linkIndex.index) {
+        if (type === 'external') {
+            result.set(url, { url });
         } else {
             let path = await getDstUrl(es, eid);
 
             path = uriToPath(path);
-            
+
             // log('[getDstUrl]', eid, path );
-            result.set(url, {url:path});
+            result.set(url, { url: path });
         }
     }
     // log('[buildPageLinks]', linkIndex.index);
@@ -164,7 +164,7 @@ export async function buildPageLinks( es:QueryableEntitySet, linkIndex:SiteIndex
  * @param fileIndex 
  * @param path 
  */
-export function getEntityImportUrlFromPath(fileIndex: SiteIndex, path: string, mimes?: string[] ) {
+export function getEntityImportUrlFromPath(fileIndex: SiteIndex, path: string, mimes?: string[]) {
     // console.log('[getEntityImportUrlFromPath]', path);
     if (fileIndex === undefined || path == '') {
         return undefined;
@@ -172,12 +172,12 @@ export function getEntityImportUrlFromPath(fileIndex: SiteIndex, path: string, m
     let entry = fileIndex.index.get(path);
     if (entry === undefined) {
         // attempt to find without ext
-        for( const [url,idxEntry] of fileIndex.index ){
+        for (const [url, idxEntry] of fileIndex.index) {
             let ext = Path.extname(url);
-            let wit = url.substring(0, url.length-ext.length);
-            if( path === wit ){
+            let wit = url.substring(0, url.length - ext.length);
+            if (path === wit) {
                 const [mime] = idxEntry;
-                if( mimes !== undefined && mimes.indexOf(mime) === -1 ){
+                if (mimes !== undefined && mimes.indexOf(mime) === -1) {
                     continue;
                 }
                 // log('[gE]', url, idxEntry);
@@ -185,7 +185,7 @@ export function getEntityImportUrlFromPath(fileIndex: SiteIndex, path: string, m
                 break;
             }
         }
-        if( entry === undefined ){
+        if (entry === undefined) {
             return undefined;
         }
     }
@@ -212,12 +212,12 @@ export async function getEntityCSSDependencies(es: QueryableEntitySet, e: Entity
 
     for (const dep of cssDeps) {
         const { src, dst } = dep.Dep;
-        let path = await getDstUrl(es,dst);
+        let path = await getDstUrl(es, dst);
 
         const com = await es.getComponent(toComponentId(dst, did));
         // log('[getEntityCSSDependencies]', dst, did, com);
 
-        if( com ){
+        if (com) {
             result.push({ path, text: com.data });
         }
     }
@@ -237,17 +237,17 @@ export async function getEntityCSSDependencies(es: QueryableEntitySet, e: Entity
  * @param options 
  * @returns 
  */
-export async function applyImports(site: Site, e: Entity, imports:ImportDescr[], options: ProcessOptions): Promise<Entity> {
+export async function applyImports(site: Site, e: Entity, imports: ImportDescr[], options: ProcessOptions): Promise<Entity> {
     const { es } = site;
-   
+
     // gather existing import and css dependencies
-    const existingIds = new Set(await getDependencyEntityIds(es, e.id, ['import','css']));
-    
+    const existingIds = new Set(await getDependencyEntityIds(es, e.id, ['import', 'css']));
+
     for (let [importEid, url, mime, specifiers] of imports) {
         let type: DependencyType = 'import';
-   
+
         const match = parseEntityUrl(url);
-   
+
         if (match !== undefined) {
             const { eid, did } = match;
             if (did === '/component/scss') {
@@ -258,12 +258,12 @@ export async function applyImports(site: Site, e: Entity, imports:ImportDescr[],
         // log('[applyImports]', url, specifiers );
 
         let coms = [];
-        coms.push( es.createComponent('/component/url', { url }) );
-        if( specifiers ){
+        coms.push(es.createComponent('/component/url', { url }));
+        if (specifiers) {
             // for now stuff into meta
-            coms.push( es.createComponent('/component/meta', {meta:{specifiers}}));
+            coms.push(es.createComponent('/component/meta', { meta: { specifiers } }));
         }
-        let depId = await insertDependency(es, e.id, importEid, type, coms );
+        let depId = await insertDependency(es, e.id, importEid, type, coms);
 
         if (existingIds.has(depId)) {
             existingIds.delete(depId);
@@ -277,12 +277,12 @@ export async function applyImports(site: Site, e: Entity, imports:ImportDescr[],
 }
 
 
-export function parseEntityUrl( url:string ){
+export function parseEntityUrl(url: string) {
     const re = new RegExp("e:\/\/([0-9]+)([-a-zA-Z0-9()@:%_+.~#?&//=]*)", "i");
-    let match = re.exec( url );
-    if( match !== null ){
-        const [ url, eid, did ] = match;
-        return { eid:toInteger(eid), did, url };
+    let match = re.exec(url);
+    if (match !== null) {
+        const [url, eid, did] = match;
+        return { eid: toInteger(eid), did, url };
     }
     return undefined;
 }
