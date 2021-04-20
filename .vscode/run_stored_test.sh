746e5b6ece26a5e642de35a33ccd034918786fd3
Alex Veenendaal <alex@opendoorgonorth.com>
2021-04-20 10:26:34 +0100

    using ts-eager as runner; massive speedup

--- .vscode/run_stored_test.sh
+++ .vscode/run_stored_test.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
 mix_arg=$( cat $1 )
-echo npx ts-node $mix_arg
-npx ts-node $mix_arg
+
+# node -r ts-eager/register myfile.ts
+echo node -r ts-eager/register $mix_arg
+node -r ts-eager/register $mix_arg
+
+# echo npx ts-node $mix_arg
+# npx ts-node $mix_arg
